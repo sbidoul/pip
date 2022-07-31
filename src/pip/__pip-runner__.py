8fa8f604ad1a487e56bb5000e4cb9e34132802a4
Stéphane Bidoul <stephane.bidoul@gmail.com>
2022-07-31 14:41:18 +0200

    Make __pip-runner__.py fail gracefully on Python 2

--- src/pip/__pip-runner__.py
+++ src/pip/__pip-runner__.py
@@ -4,26 +4,38 @@ This file is named as it is, to ensure that this module can't be imported via
 an import statement.
 """
 
-import runpy
+# /!\ This version compatibility check section must be Python 2 compatible. /!\
+
 import sys
-import types
-from importlib.machinery import ModuleSpec, PathFinder
-from os.path import dirname
-from typing import Optional, Sequence, Union
+
+# Copied from setup.py
+PYTHON_REQUIRES = (3, 7)
+
+
+def version_str(version):  # type: ignore
+    return ".".join(str(v) for v in version)
+
+
+if sys.version_info[:2] < PYTHON_REQUIRES:
+    raise SystemExit(
+        "This version of pip does not support python {} (requires >={}).".format(
+            version_str(sys.version_info[:2]), version_str(PYTHON_REQUIRES)
+        )
+    )
+
+# From here on, we can use Python 3 features, but the syntax must remain
+# Python 2 compatible.
+
+import runpy  # noqa: E402
+from importlib.machinery import PathFinder  # noqa: E402
+from os.path import dirname  # noqa: E402
 
 PIP_SOURCES_ROOT = dirname(dirname(__file__))
-# Copied from setup.py
-PYTHON_REQUIRES = ">=3.7"
 
 
 class PipImportRedirectingFinder:
     @classmethod
-    def find_spec(
-        self,
-        fullname: str,
-        path: Optional[Sequence[Union[bytes, str]]] = None,
-        target: Optional[types.ModuleType] = None,
-    ) -> Optional[ModuleSpec]:
+    def find_spec(self, fullname, path=None, target=None):  # type: ignore
         if fullname != "pip":
             return None
 
@@ -32,22 +44,8 @@ class PipImportRedirectingFinder:
         return spec
 
 
-def check_python_version() -> None:
-    # Import here to ensure the imports happen after the sys.meta_path change.
-    from pip._vendor.packaging.specifiers import SpecifierSet
-    from pip._vendor.packaging.version import Version
-
-    py_ver = Version("{0.major}.{0.minor}.{0.micro}".format(sys.version_info))
-    if py_ver not in SpecifierSet(PYTHON_REQUIRES):
-        raise SystemExit(
-            f"This version of pip does not support python {py_ver} "
-            f"(requires {PYTHON_REQUIRES})"
-        )
-
-
 # TODO https://github.com/pypa/pip/issues/11294
 sys.meta_path.insert(0, PipImportRedirectingFinder())
 
 assert __name__ == "__main__", "Cannot run __pip-runner__.py as a non-main module"
-check_python_version()
 runpy.run_module("pip", run_name="__main__", alter_sys=True)
